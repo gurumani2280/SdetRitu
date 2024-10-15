c28cf77e7e24cb84e2d11fb78869ddc0b8bf51d0
unknown <mani.2280@gmail.com>
2024-10-15 20:57:45 +0530

    interview rituraj

---
+++ Interview-Question/InterviewRituraj/rituraj1/M2.java
@@ -0,0 +1,48 @@
+package com.rituraj1;
+
+import java.util.Arrays;
+
+public class M2 {
+
+	public static void main(String[] args) {
+		// "12hello23world output = 35 // 12+23 =35 
+		String input = "12hello23world";
+		String testAlphnumberic= input.replaceAll("[a-zA-Z0-9]+", "");
+		System.out.println("-- =="  + testAlphnumberic);
+		if(testAlphnumberic.equals("")) {
+			String[] inputArraywithMultipleDigit2 = input.split("[a-zA-Z]+");
+			System.out.println(Arrays.toString(inputArraywithMultipleDigit2));
+			System.out.println(inputArraywithMultipleDigit2.length);
+			int sum = 0;
+			for(int i = 0; i < inputArraywithMultipleDigit2.length; i ++) {
+				int num = Integer.parseInt(inputArraywithMultipleDigit2[i]);
+				sum = sum + num;
+			}
+			System.out.println("sum of digits "+sum);
+		} else {
+			System.out.println("NOt a alphanumbric input");
+		}
+		
+		
+		
+		
+		
+		/*
+		 * 
+		 String[] inputArraywithMultipleDigit = input.split("\\d+");
+		System.out.println(Arrays.toString(inputArraywithMultipleDigit));
+		System.out.println(inputArraywithMultipleDigit.length);
+		/*
+		 [, hello, world]
+			3
+		 
+		 *[12, 23]
+2
+sum of digits 35
+		 */
+		
+		
+
+	}
+
+}
---
+++ Interview-Question/InterviewRituraj/rituraj1/M3.java
@@ -0,0 +1,25 @@
+package com.rituraj1;
+
+public class M3 {
+
+	public static void main(String[] args) {
+		// "12hello23world output = // 1+ 2 +2+3 = 8
+		String input = "12hello23world@";
+		if(input.matches("[a-zA-Z0-9]+")) {
+			String replacedInput = input.replaceAll("[a-zA-Z]+", "");
+			System.out.println(replacedInput);
+			System.out.println(replacedInput.length());
+			int sum = 0;
+			for(int i = 0; i < replacedInput.length(); i ++) {
+				int num = Integer.parseInt(replacedInput.charAt(i)+"");
+				sum = sum + num;
+			}
+			System.out.println("sum of digits "+sum);
+		} else {
+			System.out.println("NOt a alphanumbric input");
+		}
+		
+
+	}
+
+}
